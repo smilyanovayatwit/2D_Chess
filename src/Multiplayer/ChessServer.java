9c0e60a9e9cd019c17915bb5b2ebaf5ece5ee3d3
Tiffany Phan <phant9@wit.edu>
2020-11-23 16:03:29 -0500

    Created classes for Server-Client interaction

---
+++ src/Multiplayer/ChessServer.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package Multiplayer;
+
+
+/**
+ * 
+ *
+ * @author phant9
+ * @version 1.0.0 2020-11-23 Initial implementation
+ *
+ */
+public class ChessServer
+    {
+
+    }
+	// end class ChessServer
